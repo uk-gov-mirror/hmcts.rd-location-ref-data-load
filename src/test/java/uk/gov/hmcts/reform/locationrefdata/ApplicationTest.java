af95eaf0b0cc1a76e9af1e29ebf3a15aeb431c1f
abhijit <Abhijit.diwan@hmcts.net>
2020-08-25 13:34:24 +0100

    fix: fix smoke test

--- src/test/java/uk/gov/hmcts/reform/locationrefdata/ApplicationTest.java
+++ src/test/java/uk/gov/hmcts/reform/locationrefdata/ApplicationTest.java
@@ -2,10 +2,14 @@ package uk.gov.hmcts.reform.locationrefdata;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.times;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
 public class ApplicationTest {
 
     @Test
-    public void main() {
+    public void mainTest() {
         Application.main(new String[] {});
+        verifyStatic(Application.class,times(1));
     }
 }
