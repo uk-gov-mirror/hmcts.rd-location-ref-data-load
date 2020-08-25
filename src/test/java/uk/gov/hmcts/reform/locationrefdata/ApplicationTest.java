69706be967800a3122cb58e53e04295fe8773149
abhijit <Abhijit.diwan@hmcts.net>
2020-08-25 13:21:44 +0100

    fix: fix smoke test

---
+++ src/test/java/uk/gov/hmcts/reform/locationrefdata/ApplicationTest.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.locationrefdata;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        Application.main(new String[] {});
+    }
+}
