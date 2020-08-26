ab6f2f9549c75498ef431f77f6890b6300e39784
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-26 11:20:09 +0100

    Add dependencies

--- src/main/java/uk/gov/hmcts/reform/locationrefdata/Application.java
+++ src/main/java/uk/gov/hmcts/reform/locationrefdata/Application.java
@@ -9,23 +9,23 @@ import org.springframework.context.ApplicationContext;
 import uk.gov.hmcts.reform.health.HealthAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform", exclude = HealthAutoConfiguration.class)
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+// @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @Slf4j
 public class Application implements ApplicationRunner {
 
     private static String logComponentName = "Location Reference Data Load";
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class);
         //Sleep added to allow app-insights to flush the logs
-        Thread.sleep(7000);
+        // Thread.sleep(7000);
         int exitCode = SpringApplication.exit(context);
         log.info("{}:: Application exiting with exit code {} ", logComponentName, exitCode);
         System.exit(exitCode);
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         log.info("{}:: no run of  Application as it has ran for the day::", logComponentName);
     }
 }
