b66ae7021662bc5813f135e386fb4f8e321d9413
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-26 11:05:35 +0100

    Adding app exit code

--- src/main/java/uk/gov/hmcts/reform/locationrefdata/Application.java
+++ src/main/java/uk/gov/hmcts/reform/locationrefdata/Application.java
@@ -1,15 +1,31 @@
 package uk.gov.hmcts.reform.locationrefdata;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.ApplicationContext;
+import uk.gov.hmcts.reform.health.HealthAutoConfiguration;
 
-@SpringBootApplication
-@EnableCircuitBreaker
+@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform", exclude = HealthAutoConfiguration.class)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+@Slf4j
+public class Application implements ApplicationRunner {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+    private static String logComponentName = "Location Reference Data Load";
+
+    public static void main(final String[] args) throws Exception {
+        ApplicationContext context = SpringApplication.run(Application.class);
+        //Sleep added to allow app-insights to flush the logs
+        Thread.sleep(7000);
+        int exitCode = SpringApplication.exit(context);
+        log.info("{}:: Application exiting with exit code {} ", logComponentName, exitCode);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("{}:: no run of  Application as it has ran for the day::", logComponentName);
     }
 }
