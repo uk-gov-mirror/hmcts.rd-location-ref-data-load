a521c8dab611f99209bb813982e384be75cd277f
abhijit <Abhijit.diwan@hmcts.net>
2020-09-11 11:15:56 +0100

    feat: Create Database of the org urn microservice(RDCC-1868)

--- src/test/java/uk/gov/hmcts/reform/locationrefdata/ApplicationTest.java
+++ src/test/java/uk/gov/hmcts/reform/locationrefdata/ApplicationTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static java.lang.String.valueOf;
 import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
 
 public class ApplicationTest {
 
@@ -14,6 +16,7 @@ public class ApplicationTest {
 
     @Test
     public void mainTest() {
+        System.setProperty("FLYWAY_ENABLED", valueOf(false));
         Application.main(new String[] {});
         verifyStatic(Application.class,times(1));
     }
