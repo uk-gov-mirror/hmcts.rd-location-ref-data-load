1ef844be2d9c6e1a7d49d19d7d2edb59b8adcda0
abhijit <Abhijit.diwan@hmcts.net>
2020-10-02 16:01:46 +0100

    feat: LRD load application(RDCC-1867)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,15 +24,7 @@ data "azurerm_key_vault" "s2s_key_vault" {
   resource_group_name = "rpe-service-auth-provider-${local.local_env}"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-  tags {
-    "Deployment Environment" = "${var.env}"
-    "Team Name" = "${var.team_name}"
-    "lastUpdated" = "${timestamp()}"
-  }
-}
+
 
 data "azurerm_key_vault_secret" "s2s_url" {
   name = "s2s-url"
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,1 @@
-output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
-}
 
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
-
-output "vaultName" {
-  value = "${local.key_vault_name}"
-}
