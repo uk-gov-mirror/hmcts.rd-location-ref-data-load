469abc3d01a6af1085d2eb2ceeac21cf0bd89c65
Sabina Sharangdhar <sabina@example.com>
2026-09-21 19:05:55 +0100

    changes to accomodate new tables and new fields in court venue

--- src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdBlobSupport.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdBlobSupport.java
@@ -26,9 +26,6 @@ import static java.util.Objects.isNull;
     AzureBlobConfig.class, BlobStorageCredentials.class}, initializers = ConfigDataApplicationContextInitializer.class)
 public class LrdBlobSupport {
 
-    private static final String ACTIVE_CONTAINER_NAME = "lrd-ref-data";
-    private static final String ARCHIVE_CONTAINER_NAME = "lrd-ref-data-archive";
-
     @Autowired
     private BlobServiceClientBuilder blobServiceClientBuilder;
 
@@ -40,8 +37,6 @@ public class LrdBlobSupport {
     BlobContainerClient cloudBlobContainer;
 
     BlobContainerClient cloudBlobArchContainer;
-
-    private boolean containersCreated;
 
     @Value("${archival-date-format}")
     private String archivalDateFormat;
@@ -55,18 +50,16 @@ public class LrdBlobSupport {
             .endpoint(uri)
             .credential(credential)
             .buildClient();
-        cloudBlobContainer = cloudBlobClient.getBlobContainerClient(ACTIVE_CONTAINER_NAME);
-        cloudBlobArchContainer = cloudBlobClient.getBlobContainerClient(ARCHIVE_CONTAINER_NAME);
+        cloudBlobContainer = cloudBlobClient.createBlobContainerIfNotExists("lrd-ref-data");
+        cloudBlobArchContainer = cloudBlobClient.createBlobContainerIfNotExists("lrd-ref-data-archive");
     }
 
     public void uploadFile(String blob, InputStream sourceFile) throws Exception {
-        createContainersIfRequired();
         BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         cloudBlockBlob.upload(sourceFile);
     }
 
     public void deleteBlob(String blob, boolean... status) throws Exception {
-        createContainersIfRequired();
         Thread.sleep(1000);
         BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         if (cloudBlockBlob.exists()) {
@@ -84,16 +77,7 @@ public class LrdBlobSupport {
     }
 
     public boolean isBlobPresent(String blob) throws Exception {
-        createContainersIfRequired();
         BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         return cloudBlockBlob.exists();
-    }
-
-    private synchronized void createContainersIfRequired() {
-        if (!containersCreated) {
-            cloudBlobClient.createBlobContainerIfNotExists(ACTIVE_CONTAINER_NAME);
-            cloudBlobClient.createBlobContainerIfNotExists(ARCHIVE_CONTAINER_NAME);
-            containersCreated = true;
-        }
     }
 }
